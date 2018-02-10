4c0b022100b0cef41b8fc379e8da1c020dd112f9
Karol Baska <karolbaska@gmail.com>
2018-02-10 22:50:43 +0100

    .

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <FontSelect></FontSelect>
+  </div>
+</template>
+
+<script>
+import FontSelect from './components/FontSelect.vue';
+
+export default {
+  components: {
+    FontSelect
+  },
+}
+</script>
+
+<style>
+  body {
+    padding: 50px;
+    background-color: #333;
+  }
+
+  p,
+  h1 {
+    color: #fff;
+  }
+</style>
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import App from './App.vue'
+import FontSelect from './components/FontSelect.vue'
+
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
