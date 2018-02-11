0da5b377436abccc5bec01f3876c85646ea5c3bd
Karol Baska <karolbaska@gmail.com>
2018-02-11 20:51:20 +0100

    adding gh pages

--- src/components/FontSelect.vue
+++ src/components/FontSelect.vue
@@ -1,20 +1,25 @@
 <template>
   <div>
-    <h1><s>jQuery</s> {{title}}</h1>
-    <select  v-model="selected" :style="changeStyle(selected)">
-      <option value="" disabled hidden>Select a font</option>
-      <option v-for="font in fonts"
-      :key="font"
-      :style="changeStyle(font)">
-        {{font}}
-      </option>
-    </select>
+    <h1>{{title}}</h1>
+    <div @click="expandList" class="font-selector">
+        <span :style="changeStyle(selected)">{{fontName}}<span :class="{arrowUp: selectUp}" class="arrowDown" ></span></span>
+      <ul v-if="showList"
+          @click="expandList"
+          class="font-select">
+        <li v-for="font in fonts"
+            @click="changeFont(font)"
+            :key="font"
+            :style="changeStyle(font)">
+          {{font}}
+        </li>
+      </ul>
+    </div>
     <p :style="changeStyle(selected)">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 </div>
 </template>
 
 <script>
-const fonts =  [
+  const fonts =  [
         "Aclonica",
         "Allan",
         "Annie+Use+Your+Telescope",
@@ -233,35 +238,114 @@ const fonts =  [
         "Yeseva+One",
         "Zeyada"
 ];
-export default{
-  data() {
-    return{
-      title:'Vue Fontselect Plugin Example',
-      selected: '',
-      fonts: fonts
+  export default{
+    data() {
+      return{
+        title:'Vue Fontselect Plugin Example',
+        selected: '',
+        fonts: fonts,
+        showList: false,
+        fontName: "Select a font",
+        styleFont: '',
+        selectUp: false
+      }
+    },
+    methods: {
+      changeStyle(font){
+        const slicer = font.indexOf(':');
+        return ~slicer ?
+          this.styleFont = {fontFamily: font.slice(0,slicer), fontWeight: font.slice(slicer+1,slicer+4)} :
+          this.styleFont={fontFamily: font, fontWeight: 400};
+      },
+      expandList(){
+      this.selectUp = !this.showList;
+      this.showList = !this.showList;
+      },
+    changeFont(value){
+      this.fontName= value;
+      this.selected = this.fontName;
+      this.showList = false;
+      }
+    },
+    created(){
+        const link = document.createElement('link');
+        link.type = "text/css";
+        link.rel = "stylesheet";
+        link.href = `https://fonts.googleapis.com/css?family=${fonts.join().replace(/,/g , '|')}`;
+        document.head.appendChild(link);
+        this.fonts.forEach((el, i, arr) =>
+          arr[i] = el.replace(/\+/g, " ")
+        )
     }
-  },
-  methods: {
-    changeStyle(font){
-      font = font.split('+').join(' ');
-      const slicer = font.indexOf(':');
-      return ~slicer ?
-        {fontFamily: font.slice(0,slicer), fontWeight: font.slice(slicer+1,slicer+4)} :
-        {fontFamily: font, fontWeight: 400};
-    }
-  },
-  created(){
-      const link = document.createElement('link');
-      link.type = "text/css";
-      link.rel = "stylesheet";
-      link.href = `https://fonts.googleapis.com/css?family=${fonts.join().replace(/,/g , '|')}`;
-      document.head.appendChild(link);
-      this.fonts.forEach((el, i, arr) =>
-        arr[i] = el.replace(/\+/g, " ")
-      )
-  }
   }
 </script>
 
 <style>
+
+.font-selector {
+  position: relative;
+  background-color: white;
+  width: 210px;
+  border: 1px solid black;
+  -webkit-border-radius: 5px;
+  -moz-border-radius: 5px;
+  border-radius: 5px;
+  padding: 5px;
+  cursor: pointer;
+}
+
+
+.arrowDown {
+  float: right;
+  background-image: url(../assets/fs-sprite.png);
+  background-position: 15% -50%;
+  background-repeat: no-repeat;
+  width: 5%;
+  height: 30px;
+}
+
+.arrowDown::after,
+.arrowDown::before {
+  content: '';
+  clear: both;
+}
+
+.arrowUp {
+  float: right;
+  background-image: url(../assets/fs-sprite.png);
+  background-position: 85% -50%;
+  background-repeat: no-repeat;
+  width: 5%;
+  height: 30px;
+}
+
+.font-select {
+  box-sizing: border-box;
+  position: absolute;
+  background-color: white;
+  width: 210px;
+  list-style-type: none;
+  margin: 0;
+  padding-left: 4px;
+  left: 0;
+  max-height: 180px;
+  width: 100%;
+  overflow-y: scroll;
+  -webkit-border-radius: 0 0 5px 5px;
+  -moz-border-radius: 0 0 5px 5px;
+  border-radius: 0 0 5px 5px;
+  border-right: 2px solid rgb(170, 170, 170);
+  border-left: 2px solid white;
+}
+
+ul > li {
+  padding-top: 5px;
+  padding-bottom: 5px;
+}
+
+ul > li:hover {
+  background-color: #5F9EA0;
+  color: white;
+}
+
 </style>
